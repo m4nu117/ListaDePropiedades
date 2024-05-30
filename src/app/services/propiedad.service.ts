483c6764298080bbbe323840e50b32895ba323f8
Manuel <you@example.com>
2024-05-29 22:26:42 -0500

    Correcion delete y actulizar

--- src/app/services/propiedad.service.ts
+++ src/app/services/propiedad.service.ts
@@ -13,15 +13,15 @@ export class PropiedadService {
 
   }
   get(id: number) {
-    return this.http.get<Propiedad>('http://localhost:8080/api/RegisterProperty/${id}')
+    return this.http.get<Propiedad>(`http://localhost:8080/api/RegisterProperty/${id}`);
   }
   create (propiedad: any) {
-    return this.http.post<Propiedad>('http://localhost:8080/api/RegisterProperty', propiedad)
+    return this.http.post<Propiedad>('http://localhost:8080/api/RegisterProperty', propiedad);
   }
   update (id:number, propiedad: any) {
-    return this.http.put<Propiedad>('http://localhost:8080/api/RegisterProperty/${id}', propiedad)
+    return this.http.put<Propiedad>(`http://localhost:8080/api/RegisterProperty/${id}`, propiedad);
   }
   delete (id:number) {
-    return this.http.delete<void>('http://localhost:8080/api/RegisterProperty/${id}');
+    return this.http.delete<void>(`http://localhost:8080/api/RegisterProperty/${id}`);
   }
 }
